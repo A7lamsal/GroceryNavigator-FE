26af65b570e12176f0600b3d022c55e88856cfb0
Ahlam Saleh <Ahlam.saleh321@gmail.com>
2023-04-12 02:04:07 +0300

    tested files

--- App.tsx
+++ App.tsx
@@ -1,81 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import {
- // StyleSheet,
- // Text,
-  View,
-  //ImageBackground,
-  //Pressable,
-  //Alert,
-} from "react-native";
+import { StyleSheet, View } from "react-native";
 import Categorise from "./src/app/components/screens/categorise";
-//import Branch from "./src/app/components/screens/branch";
-//import Store from "./src/app/components/screens/store";
-// import arrow from "../GroceryNavigator-FE/src/assets/icons/arrowleft.svg"
-//fff
+import Branch from "./src/app/components/screens/branch";
+import Store from "./src/app/components/screens/store";
 
-
-export default function App(){
-
-  return(
-    <View>
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <Store /> */}
+      {/* <Branch /> */}
       <Categorise />
     </View>
   );
 }
-{/* 
-export default function App() {
-  const img = {
-    uri: "../GroceryNavigator-FE/src/assets/icons/arrowleft.svg",
-  };
-  return (
-    <View style={styles.container}>
-      <Text style={styles.containerText}>First let's Select the Store</Text>
-      <StatusBar style="auto" />
-      <View>
-        <Store />
-      </View>
 
-      <Pressable style={styles.button}>
-        <Text style={styles.text}>Next</Text>
-      </Pressable>
-      {/* <Branch /> */}
-    
-  
-   // </View>
-  //);
-  //}
-
-
-//const styles = StyleSheet.create({
- // container: {
-   // flex: 1,
-   // gap: 16,
-   // backgroundColor: "#fff",
-   // marginLeft: 15,
-   // marginTop: 25,
- // },
-  //image: {
-    // flex: 1,
-   // resizeMode: "cover",
-   // justifyContent: "center",
-    //height: "auto",
-  //},
- // button: {
-   // borderRadius: 4,
-   // elevation: 3,
-   // backgroundColor: "#6B3EC6",
-    //paddingVertical: 12,
-   // paddingHorizontal: 32,
-   // width: "50%",
-   // marginRight: "auto",
-   // marginLeft: "auto",
-  //},
-  //text: {
-  //  color: "white",
-  //  marginRight: "auto",
-  //  marginLeft: "auto",
-  //},
-  //containerText: {
-  //  fontWeight: "bold",
-  //},
-//});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 16,
+    backgroundColor: "#fff",
+    marginLeft: 15,
+    marginTop: 25,
+  },
+});
