698cdedbec96d8f6c12e423554a1cd12c143f714
Ahlam Saleh <Ahlam.saleh321@gmail.com>
2023-04-12 02:03:22 +0300

    formatted doc

--- src/app/components/screens/categorise.tsx
+++ src/app/components/screens/categorise.tsx
@@ -1,37 +1,22 @@
 //import React from 'react';
-import { View, Text, StyleSheet,Image } from 'react-native';
+import { View, Text, StyleSheet, Image } from "react-native";
 
-export default function Categorise(props:any) {
-    return (
-        <View style={styles.contener}>
-            <View style={styles.headerleft}>
-                <Image source={require('../../../assets/images/Vector.png')}/>
-            </View>
-            <Text>Products</Text>
-            <View style={styles.headerright}>
-              <Image source={require('../../../assets/images/shoppingcart.png')}/>
-            </View>
-        </View>
-    );
+export default function Categorise(props: any) {
+  return (
+    <View style={styles.contener}>
+      <View style={styles.headerleft}>
+        <Image source={require("../../../assets/images/Vector.png")} />
+      </View>
+      <Text>Products</Text>
+      <View style={styles.headerright}>
+        <Image source={require("../../../assets/images/shoppingcart.png")} />
+      </View>
+    </View>
+  );
 }
 
-const styles=StyleSheet.create({
-
-   contener:{
-
-   },
-   headerleft:{
-
-
-   },
-   headerright:{
-
-
-   },
-
-
-})
-
-
-
-
+const styles = StyleSheet.create({
+  contener: {},
+  headerleft: {},
+  headerright: {},
+});
