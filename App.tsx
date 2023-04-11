14cfd9f8214c88f1a7c89f50b2df0284cfffada3
KhadijahFH <munfh-amani@hotmail.com>
2023-04-12 01:00:18 +0300

    comented things

--- App.tsx
+++ App.tsx
@@ -1,16 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import {
-  StyleSheet,
-  Text,
+ // StyleSheet,
+ // Text,
   View,
-  ImageBackground,
-  Pressable,
-  Alert,
+  //ImageBackground,
+  //Pressable,
+  //Alert,
 } from "react-native";
-import Branch from "./src/app/components/screens/branch";
-import Store from "./src/app/components/screens/store";
+import Categorise from "./src/app/components/screens/categorise";
+//import Branch from "./src/app/components/screens/branch";
+//import Store from "./src/app/components/screens/store";
 // import arrow from "../GroceryNavigator-FE/src/assets/icons/arrowleft.svg"
 
+
+
+export default function App(){
+
+  return(
+    <View>
+      <Categorise />
+    </View>
+  );
+}
+{/* 
 export default function App() {
   const img = {
     uri: "../GroceryNavigator-FE/src/assets/icons/arrowleft.svg",
@@ -27,40 +39,43 @@ export default function App() {
         <Text style={styles.text}>Next</Text>
       </Pressable>
       {/* <Branch /> */}
-    </View>
-  );
-}
+    
+  
+   // </View>
+  //);
+  //}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    gap: 16,
-    backgroundColor: "#fff",
-    marginLeft: 15,
-    marginTop: 25,
-  },
-  image: {
+
+//const styles = StyleSheet.create({
+ // container: {
+   // flex: 1,
+   // gap: 16,
+   // backgroundColor: "#fff",
+   // marginLeft: 15,
+   // marginTop: 25,
+ // },
+  //image: {
     // flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    height: "auto",
-  },
-  button: {
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: "#6B3EC6",
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    width: "50%",
-    marginRight: "auto",
-    marginLeft: "auto",
-  },
-  text: {
-    color: "white",
-    marginRight: "auto",
-    marginLeft: "auto",
-  },
-  containerText: {
-    fontWeight: "bold",
-  },
-});
+   // resizeMode: "cover",
+   // justifyContent: "center",
+    //height: "auto",
+  //},
+ // button: {
+   // borderRadius: 4,
+   // elevation: 3,
+   // backgroundColor: "#6B3EC6",
+    //paddingVertical: 12,
+   // paddingHorizontal: 32,
+   // width: "50%",
+   // marginRight: "auto",
+   // marginLeft: "auto",
+  //},
+  //text: {
+  //  color: "white",
+  //  marginRight: "auto",
+  //  marginLeft: "auto",
+  //},
+  //containerText: {
+  //  fontWeight: "bold",
+  //},
+//});
