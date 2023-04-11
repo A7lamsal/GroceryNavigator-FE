d25bb5946d55051a9f666c6738149289d6f6b627
Ahlam Saleh <Ahlam.saleh321@gmail.com>
2023-04-11 04:02:09 +0300

    created store page

--- src/app/components/screens/store.tsx
+++ src/app/components/screens/store.tsx
@@ -1,25 +1,38 @@
-import { View, Text, Image } from "react-native";
+import { View, ImageBackground, Image, SafeAreaView } from "react-native";
 import { StyleSheet } from "react-native";
 
-export default function Store() {
-  return <View style={styles.cardContainer}></View>;
+export default function Store(props: any) {
+  return (
+    <View>
+      <SafeAreaView>
+        <Image
+          source={require("../../../assets/images/danube.png")}
+          style={styles.Image}
+        />
+      </SafeAreaView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  cardContainer: {
-    width: 186,
-    height: 180,
-    borderRadius: 20,
-    backgroundColor: "#a29bfe",
+  // cardContainer: {
+  //   width: 186,
+  //   height: 180,
+  //   borderRadius: 20,
+  //   // backgroundColor: "#a29bfe",
 
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 5,
-      height: 5,
-    },
+  //   // shadowColor: "#000",
+  //   shadowOffset: {
+  //     width: 5,
+  //     height: 5,
+  //   },
 
-    shadowOpacity: 0.75,
-    shadowRadius: 5,
-    elevation: 9,
+  //   shadowOpacity: 0.75,
+  //   shadowRadius: 5,
+  //   elevation: 9,
+  // },
+  Image: {
+    width: 190,
+    height: 200,
   },
 });
